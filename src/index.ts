853d26956c74dec7877f8a0b0fe4c28a87ee6725
Kai Mayer <kai.mayer@t-online.de>
2022-06-27 11:19:06 +0200

    send fcm

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { Router } from "itty-router";
-
 import Notification from "./notification";
 import { getUid } from "./random";
 import { base64ToUint8Array } from "./convert";
+import { firebase } from "./firebase";
 /**
  * Welcome to Cloudflare Workers! This is your first worker.
  *
@@ -16,6 +16,7 @@ import { base64ToUint8Array } from "./convert";
 export interface Env {
   // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
   // MY_KV_NAMESPACE: KVNamespace;
+  FCM_AUTH: string;
   IMAGES: KVNamespace;
   //
   // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
@@ -43,7 +44,7 @@ router
     }
     return new Response("Bad Request", { status: 400 });
   })
-  .get("/", () => new Response("dieklingel.com"))
+  .get("/", (request: Request) => new Response(request.url))
   .get("*", () => new Response("Not Found.", { status: 404 }));
 
 router
@@ -64,8 +65,42 @@ async function sendToFcm(notification: Notification, env: Env): Promise<void> {
   if (notification.image) {
     await env.IMAGES.put(uid, notification.image, { expirationTtl: 60 });
   }
-  console.log(uid);
+  firebase.init(JSON.parse(env.FCM_AUTH));
   // send notification
+  let token: any = await firebase.getAccessToken();
+  let img_url = "https://fcm-worker.dieklingel.workers.dev/img/" + uid;
+  for (let device_token of notification.tokens) {
+    let fcm = {
+      "message": {
+        "token": device_token,
+        "android": {
+          "notification": {
+            "title": notification.title,
+            "body": notification.body,
+            "sound": "ringtone",
+            "icon": "icon_16x16"
+          }
+        },
+        "apns": {
+          "payload": {
+            "aps": {
+              "alert": {
+                "title": notification.title,
+                "subtitle": "dieklingel.com",
+                "body": notification.body,
+              },
+              "sound": "ringtone.wav",
+              "mutable-content": 1
+            }
+          },
+          "fcm_options": {
+            "image": img_url,
+          }
+        }
+      }
+    };
+    firebase.sendMessage(fcm, token["access_token"]);
+  }
 }
 
 async function fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
